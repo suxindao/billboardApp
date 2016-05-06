2c2f7dd8bb1c1f28d574298f4f7690df76d742bc
suxindao <xindao.su@healthcrosscn.com>
2016-05-06 10:12:27 +0800

    update .gitignore file

--- www/js/services.js
+++ www/js/services.js
@@ -12,7 +12,7 @@ angular.module('starter.services', [])
         var deferred = $q.defer();
         var promise = deferred.promise;
 
-        if (name == 'user' && pw == 'secret') {
+        if (name === 'user' && pw === 'secret') {
           deferred.resolve('Welcome ' + name + '!');
         } else {
           deferred.reject('Wrong credentials.');
